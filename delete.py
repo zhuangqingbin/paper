dcabc783b631dcb59128f5b38fd1ffbdd1750b81
MacJimmy <360650538@qq.com>
2019-11-28 19:06:05 +0800

    all

--- delete.py
+++ delete.py
@@ -4,8 +4,8 @@ import shutil
 dirs = ['data', 'figures', 'models', 'records']
 
 def delete_dir(path):
-    shutil.rmtree(dirpath)  # 能删除该文件夹和文件夹下所有文件
-    os.mkdir(dirpath)
+    shutil.rmtree(path)  # 能删除该文件夹和文件夹下所有文件
+    os.mkdir(path)
 
 for dir in dirs:
     delete_dir(os.path.join(os.getcwd(),dir))
